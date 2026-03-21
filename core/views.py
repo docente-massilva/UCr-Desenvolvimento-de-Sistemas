83e1ecfaeae8568432d0ec4ffe5c25582899690a
Marcos A. G. de S. Silva <marcos.a.silva6@ba.docente.senai.br>
2026-03-21 11:41:23 -0300

    Add templates no app core

--- core/views.py
+++ core/views.py
@@ -1,8 +1,4 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 def home(request):
-    return HttpResponse(
-        '<h1>Hello, World!</h1>'
-        '<p>Essa é a minha primeira página com Django</p>'
-    )
+    return render(request, 'index.html')
